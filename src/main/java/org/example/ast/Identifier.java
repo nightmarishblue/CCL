84d7faf1bcabe1ae58acde5b4019b3b2fb37d2b3
blue <midnight@nightmarish.blue>
2025-11-11 19:17:53 +0000

    Switch Identifier to record

--- src/main/java/org/example/ast/Identifier.java
+++ src/main/java/org/example/ast/Identifier.java
@@ -2,23 +2,9 @@ package org.example.ast;
 
 import java.util.Locale;
 
-public class Identifier {
-    public final String value;
-
-    public Identifier(String name) {
-        value = name.toUpperCase(Locale.ROOT);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (!(obj instanceof Identifier identifier)) return false;
-        return value.equals(identifier.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return value.hashCode();
+public record Identifier(String value) {
+    public Identifier(String value) {
+        this.value = value.toUpperCase(Locale.ROOT);
     }
 
     @Override
