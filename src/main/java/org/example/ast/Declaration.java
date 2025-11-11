34df12dc0438ba886fad231d80e19d7a0beae9a9
blue <midnight@nightmarish.blue>
2025-11-11 17:03:23 +0000

    Add type factories

--- src/main/java/org/example/ast/Declaration.java
+++ src/main/java/org/example/ast/Declaration.java
@@ -1,10 +1,17 @@
 package org.example.ast;
 
-import org.antlr.v4.runtime.ParserRuleContext;
+import org.example.grammar.CCLParser;
 
 public abstract class Declaration extends Node {
-    public Declaration(ParserRuleContext ctx) {
+    final Identifier name;
+    final Type type;
+
+    public Declaration(CCLParser.DeclarationContext ctx) {
         super(ctx);
+        final CCLParser.VariableContext variable = ctx.getRuleContext(CCLParser.VariableContext.class, 0);
+
+        name = new Identifier(variable.name.getText());
+        type = Type.fromContext(variable.type());
     }
     // TODO add subclasses for const and var declarations?
 }
---
+++ src/main/java/org/example/ast/Type.java
@@ -0,0 +1,24 @@
+package org.example.ast;
+
+import org.antlr.v4.runtime.Token;
+import org.example.grammar.CCLParser;
+
+public enum Type {
+    VOID,
+    INTEGER,
+    BOOLEAN;
+
+    static Type fromToken(final Token token) {
+        int index = token.getType();
+        return switch (index) {
+            case CCLParser.KW_VOID -> VOID;
+            case CCLParser.KW_INTEGER -> INTEGER;
+            case CCLParser.KW_BOOLEAN -> BOOLEAN;
+            default -> throw new IllegalArgumentException(String.format("Token %s does not represent a CCL type", token));
+        };
+    }
+
+    static Type fromContext(final CCLParser.TypeContext context) {
+        return fromToken(context.value);
+    }
+}
