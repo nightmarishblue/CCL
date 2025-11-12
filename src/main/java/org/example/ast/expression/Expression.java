529ee952d8623d73983586662ceab4f6446d7e14
blue <midnight@nightmarish.blue>
2025-11-12 16:36:42 +0000

    Properly support subexpressions

--- src/main/java/org/example/ast/expression/Expression.java
+++ src/main/java/org/example/ast/expression/Expression.java
@@ -8,13 +8,10 @@ public abstract class Expression extends Node {
         super(ctx);
     }
 
-    // nested expressions are only syntactically useful, not semantic - so they don't get their own class
-    public Expression(CCLParser.SubExpressionContext ctx) {
-        this(ctx.expression());
-    }
-
     public static <T extends CCLParser.ExpressionContext> Expression fromContext(T ctx) {
         return switch (ctx) {
+            // subexpressions are purely syntactic constructs, so they don't get their own class
+            case CCLParser.SubExpressionContext ctx_ -> Expression.fromContext(ctx_.expression());
             case CCLParser.ArithmeticExpressionContext ctx_ -> Arithmetic.fromContext(ctx_);
             // TODO add the rest
 //            case CCLParser.ExpressionContext ignored ->
