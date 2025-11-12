48a64fb7d660e3c470f693f4383a8e38d8326688
blue <midnight@nightmarish.blue>
2025-11-12 21:14:33 +0000

    Make the printing more readable

--- src/main/java/org/example/ast/Node.java
+++ src/main/java/org/example/ast/Node.java
@@ -4,6 +4,8 @@ import org.antlr.v4.runtime.ParserRuleContext;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public abstract class Node {
     public final int position; // or something?
@@ -15,13 +17,14 @@ public abstract class Node {
     public String toString() {
         final String type = getClass().getSimpleName();
         Field[] fields = getClass().getDeclaredFields();
-        Object[] values = Arrays.stream(fields).map(field -> {
+        Stream<Object> values = Arrays.stream(fields).map(field -> {
             try {
                 return field.get(this);
             } catch (IllegalAccessException e) {
                 throw new RuntimeException(e);
             }
-        }).toArray();
-        return String.format("%s(%s)", type, Arrays.toString(values));
+        });
+        String list = values.map(String::valueOf).collect(Collectors.joining(", "));
+        return String.format("%s(%s)", type, list);
     }
 }
