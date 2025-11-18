e12ee4a743c195b0a23b84338fa09867d353370c
blue <midnight@nightmarish.blue>
2025-11-18 15:42:59 +0000

    Add children()

--- src/main/java/org/example/ast/node/Node.java
+++ src/main/java/org/example/ast/node/Node.java
@@ -1,11 +1,35 @@
 package org.example.ast.node;
 
 import java.lang.reflect.Field;
-import java.util.Arrays;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public abstract class Node {
+    // get all the Nodes underneath this one
+    public List<Node> children() {
+        // would be safer to mandate children to define this, but reflection is a decent fallback
+        List<Node> result = new ArrayList<>();
+
+        for (Field field : this.getClass().getFields()) {
+            if (!field.canAccess(this)) continue;
+
+            Object value;
+            try { value = field.get(this); }
+            catch (IllegalAccessException e) { throw new RuntimeException(e); }
+
+            if (value instanceof Node node) result.add(node);
+            else if (value instanceof List<?> list) {
+                list.stream()
+                        .filter(Node.class::isInstance)
+                        .map(Node.class::cast)
+                        .forEach(result::add);
+            }
+        }
+
+        return Collections.unmodifiableList(result);
+    }
+
     @Override
     public String toString() {
         final String type = getClass().getSimpleName();
