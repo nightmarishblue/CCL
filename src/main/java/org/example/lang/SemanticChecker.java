f08dc17e175f530753438951586e7f96a14650ba
blue <midnight@nightmarish.blue>
2025-11-18 18:57:28 +0000

    Add checks for identifiers and scope

--- src/main/java/org/example/lang/SemanticChecker.java
+++ src/main/java/org/example/lang/SemanticChecker.java
@@ -2,11 +2,10 @@ package org.example.lang;
 
 import org.example.ast.AstVisitor;
 import org.example.ast.data.Identifier;
-import org.example.ast.node.Function;
-import org.example.ast.node.Main;
-import org.example.ast.node.Node;
-import org.example.ast.node.Program;
-import org.example.helper.Option;
+import org.example.ast.data.Variable;
+import org.example.ast.node.*;
+import org.example.ast.node.atom.Reference;
+import org.example.ast.node.declaration.Declaration;
 
 public class SemanticChecker extends AstVisitor<Void> {
     private final SymbolTable<Void> environment = new SymbolTable<>();
@@ -26,13 +25,19 @@ public class SemanticChecker extends AstVisitor<Void> {
     }
 
     private void add(final Identifier id) {
+        if (environment.peekScope().containsKey(id)) {
+            System.err.printf("Identifier %s is already defined in this scope", id);
+        }
         environment.putSymbol(id, null);
     }
 
-    private Option<Void> get(final Identifier id) {
-        return environment.getSymbol(id);
+    private void get(final Identifier id) {
+        if (!environment.getSymbol(id).present()) {
+            System.err.printf("Identifier %s is undefined\n", id);
+        }
     }
 
+    // create & destroy scopes
     @Override
     public Void visitProgram(Program node) {
         wrapScope(node);
@@ -47,7 +52,32 @@ public class SemanticChecker extends AstVisitor<Void> {
 
     @Override
     public Void visitFunction(Function node) {
-        wrapScope(node);
+        add(node.name); // functions add their name to the parent scope
+        push();
+        // functions add their parameters to their scope
+        node.parameters.stream().map(Variable::name).forEach(this::add);
+        visitChildren(node);
+        pop();
         return null;
+    }
+
+    // declarations are the only other source of symbols in the table
+    @Override
+    public Void visitDeclaration(Declaration node) {
+        add(node.variable.name());
+        return super.visitDeclaration(node);
+    }
+
+    @Override
+    public Void visitReference(Reference node) {
+        get(node.variable);
+        return super.visitReference(node);
+    }
+
+    @Override
+    public Void visitCall(Call node) {
+        get(node.function);
+        node.arguments.forEach(this::get);
+        return super.visitCall(node);
     }
 }
