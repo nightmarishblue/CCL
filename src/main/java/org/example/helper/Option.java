c27fcf731847184801f23259095a3142afc6a520
blue <midnight@nightmarish.blue>
2025-11-19 15:11:28 +0000

    Add more methods to Option

--- src/main/java/org/example/helper/Option.java
+++ src/main/java/org/example/helper/Option.java
@@ -6,8 +6,16 @@ import java.util.function.Function;
 // Java's Optional is subpar, so I have shamelessly copied Rust's
 public sealed interface Option<T> permits Option.Some, Option.None {
     boolean present();
+
+    // return the contained value if Some, throw if None
     T get();
-    <N> Option<N> map(Function<T, N> f); // maps T in Some<T> to type N, returning None if empty
+    // return the contained value if Some, or the provided default if None
+    T getOr(T fallback);
+
+    // maps Some<T> to Some<N> with the provided function, returning None if empty
+    <N> Option<N> map(Function<T, N> f);
+    // maps Some<T> to N with the provided function, returning the fallback if empty
+    <N> N mapOr(N fallback, Function<T, N> f);
 
     static <T> Some<T> some(T value) {
         return new Some<>(value);
@@ -27,8 +35,18 @@ public sealed interface Option<T> permits Option.Some, Option.None {
         }
 
         @Override
+        public T getOr(T fallback) {
+            return value;
+        }
+
+        @Override
         public <N> Option<N> map(Function<T, N> f) {
             return Option.some(f.apply(this.value));
+        }
+
+        @Override
+        public <N> N mapOr(N fallback, Function<T, N> f) {
+            return f.apply(value);
         }
 
         @Override
@@ -49,8 +67,18 @@ public sealed interface Option<T> permits Option.Some, Option.None {
         }
 
         @Override
+        public T getOr(T fallback) {
+            return fallback;
+        }
+
+        @Override
         public <N> Option<N> map(Function<T, N> f) {
             return Option.none();
+        }
+
+        @Override
+        public <N> N mapOr(N fallback, Function<T, N> f) {
+            return fallback;
         }
 
         @Override
