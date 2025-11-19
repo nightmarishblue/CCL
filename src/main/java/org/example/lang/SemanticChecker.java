a0def7ce61cf732126be2aa4db06c77bf5920f88
blue <midnight@nightmarish.blue>
2025-11-19 10:51:35 +0000

    Inline the SemanticChecker's data
    Add a const reassignment and print and incorrect variable in the example

--- src/main/java/org/example/lang/SemanticChecker.java
+++ src/main/java/org/example/lang/SemanticChecker.java
@@ -13,10 +13,11 @@ import org.example.helper.Option;
 import org.example.helper.Pair;
 import org.example.helper.Util;
 
+import java.util.List;
 import java.util.function.BiConsumer;
 
 public class SemanticChecker extends AstVisitor<Void> {
-    private final SymbolTable<Signature> environment = new SymbolTable<>();
+    private final SymbolTable<Data> environment = new SymbolTable<>();
     public final BiConsumer<Node, String> onError;
 
     public SemanticChecker(BiConsumer<Node, String> onError) {
@@ -41,13 +42,13 @@ public class SemanticChecker extends AstVisitor<Void> {
         environment.popScope();
     }
 
-    private boolean add(final Identifier id, Signature signature) {
+    private boolean add(final Identifier id, Data data) {
         if (environment.peekScope().containsKey(id)) return false;
-        environment.putSymbol(id, signature);
+        environment.putSymbol(id, data);
         return true;
     }
 
-    private Option<Signature> get(final Identifier id) {
+    private Option<Data> get(final Identifier id) {
         return environment.getSymbol(id);
     }
 
@@ -75,7 +76,7 @@ public class SemanticChecker extends AstVisitor<Void> {
     @Override
     public Void visitFunction(Function function) {
         // functions add their name to the parent scope...
-        if (!add(function.name, Signature.Function.of(function)))
+        if (!add(function.name, Data.Function.of(function)))
             alreadyDefined(function, function.name);
         push();
         // ...and add their parameters to their own scope
@@ -83,7 +84,7 @@ public class SemanticChecker extends AstVisitor<Void> {
             if (var.type() == Type.VOID)
                 error(function, String.format("Function parameter %s cannot be declared with type VOID", var.name()));
             else {
-                if (!add(var.name(), new Signature.Variable(var.type(), false)))
+                if (!add(var.name(), new Data.Variable(var.type(), false)))
                     alreadyDefined(function, var.name());
             }
         }
@@ -100,7 +101,7 @@ public class SemanticChecker extends AstVisitor<Void> {
         if (var.type() == Type.VOID)
             error(node, String.format("Variable %s cannot be declared with type void", var.name()));
         else {
-            if (!add(var.name(), new Signature.Variable(var.type(), node instanceof Const)))
+            if (!add(var.name(), new Data.Variable(var.type(), node instanceof Const)))
                 alreadyDefined(node, var.name());
         }
         return null;
@@ -114,22 +115,22 @@ public class SemanticChecker extends AstVisitor<Void> {
 
     @Override
     public Void visitCall(Call node) {
-        final Option<Signature> functionSignature = get(node.function);
+        final Option<Data> functionSignature = get(node.function);
         if (!functionSignature.present()) {
             undefined(node, node.function);
             return null;
         }
 
-        if (functionSignature.get() instanceof Signature.Function function) {
+        if (functionSignature.get() instanceof Data.Function function) {
             if (function.parameterTypes().size() != node.arguments.size())
                 error(node, String.format("Function %s called with the wrong number of arguments", node.function));
 
             for (Pair<Type, Identifier> pair : Util.zip(function.parameterTypes(), node.arguments)) {
                 final Type required = pair.left();
                 final Identifier argument = pair.right();
-                final Option<Signature> signature = get(argument);
+                final Option<Data> signature = get(argument);
                 if (!signature.present()) error(node, String.format("Argument %s is undefined", argument));
-                else if (signature.get() instanceof Signature.Variable variable && variable.type() != required)
+                else if (signature.get() instanceof Data.Variable variable && variable.type() != required)
                     error(node, String.format("Argument %s is of type %s, required type is %s", argument, variable.type(), required));
                 else error(node, String.format("Argument %s is not a variable", argument));
             }
@@ -139,13 +140,27 @@ public class SemanticChecker extends AstVisitor<Void> {
 
     @Override
     public Void visitAssign(Assign node) {
-        Option<Signature> signature = get(node.variable);
+        Option<Data> signature = get(node.variable);
         if (signature.present()) {
-            if (signature.get() instanceof Signature.Variable variable) {
+            if (signature.get() instanceof Data.Variable variable) {
                 if (variable.constant()) error(node, String.format("%s is constant and cannot be reassigned", node.variable));
                 // TODO check that the expression is of the correct type
             }
         } else undefined(node, node.variable);
         return null;
+    }
+
+    public sealed interface Data {
+        record Variable(Type type, boolean constant) implements Data {
+            public Variable {
+                if (type == Type.VOID) throw new IllegalArgumentException("Variables may not be type void");
+            }
+        }
+
+        record Function(Type type, List<Type> parameterTypes) implements Data {
+            public static Function of(org.example.ast.node.Function function) {
+                return new Function(function.type, function.parameters.stream().map(org.example.ast.data.Variable::type).toList());
+            }
+        }
     }
 }
